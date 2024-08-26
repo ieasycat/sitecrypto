a5d179466a16755aa3b39837ad3a43decf1cfe5f
 Anton Klimenka <anton@Antons-MacBook-Pro.local>
2024-08-26 20:47:14 +0200

    add views and urls

--- sitecrypto/crypto/views.py
+++ sitecrypto/crypto/views.py
@@ -1,3 +1,52 @@
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
-# Create your views here.
+menu = [
+    {'title': 'About the site', 'url_name': 'about'},
+    {'title': 'Add an article', 'url_name': 'add_page'},
+    {'title': 'Contact', 'url_name': 'contact'},
+    {'title': 'LogIn', 'url_name': 'login'},
+]
+
+data_db = [
+    {'id': 1, 'title': 'Bitcoin', 'content': 'Info about the Bitcoin', 'is_published': True},
+    {'id': 2, 'title': 'Ethereum', 'content': 'Info about the Ethereum', 'is_published': True},
+    {'id': 3, 'title': 'TON', 'content': 'Info about the TON', 'is_published': True},
+]
+
+
+def index(request):
+    data = {
+        'title': 'Main page',
+        'menu': menu,
+        'posts': data_db
+    }
+    return render(request, 'crypto/index.html', context=data)
+
+
+def show_post(request, post_id):
+    return HttpResponse(f'Show the post with ID = {post_id}')
+
+
+def about(request):
+    data = {
+        'title': 'About the site',
+        'menu': menu,
+    }
+    return render(request, 'crypto/about.html', context=data)
+
+
+def addpage(request):
+    return HttpResponse('Add the post')
+
+
+def contact(request):
+    return HttpResponse('Contact')
+
+
+def login(request):
+    return HttpResponse('Authorisation')
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
--- sitecrypto/sitecrypto/urls.py
+++ sitecrypto/sitecrypto/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from crypto.views import page_not_found
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('crypto/', include('crypto.urls')),
 ]
+
+handler404 = page_not_found
