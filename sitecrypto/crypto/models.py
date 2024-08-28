ed8d7a763545623ab2fe293decf3d47a2348abd7
Anton <73560171+ieasycat@users.noreply.github.com>
2024-08-29 00:39:53 +0400

    Update models.py

--- sitecrypto/crypto/models.py
+++ sitecrypto/crypto/models.py
@@ -5,5 +5,5 @@ class Crypto(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
-    time_update= models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now_add=True)
     is_published = models.BooleanField(default=True)
