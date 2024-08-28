41fb6dffcd3130a9e538569132103dec6cde8c45
 Anton Klimenka <anton@Antons-MacBook-Pro.local>
2024-08-28 22:39:01 +0200

    Create crypto model

--- sitecrypto/crypto/models.py
+++ sitecrypto/crypto/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Crypto(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update= models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(default=True)
