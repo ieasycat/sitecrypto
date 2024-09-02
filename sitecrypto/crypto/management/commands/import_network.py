8d9296508c50fe3f65bf11fdb13ce1fc3bedb3af
Anton <ieasycat@hotmail.com>
2024-09-02 15:26:48 +0200

    Add a script and data for its operation, which creates network models

---
+++ sitecrypto/crypto/management/commands/import_network.py
@@ -0,0 +1,29 @@
+import json
+from django.core.management.base import BaseCommand
+from crypto.models import Network
+
+
+class Command(BaseCommand):
+    help = 'Load networks data from JSON file'
+
+    def add_arguments(self, parser):
+        parser.add_argument('json_file', type=str)
+
+    def handle(self, *args, **kwargs):
+        json_file = kwargs['json_file']
+        with open(json_file, 'r') as file:
+            data = json.load(file)
+
+            for item in data:
+                network, created = Network.objects.update_or_create(
+                    id=item['id'],
+                    defaults={
+                        'title': item['title'],
+                        'full_name': item['full_name'],
+                        'slug': item['slug']
+                    }
+                )
+                if created:
+                    self.stdout.write(self.style.SUCCESS(f'Created network: {network.id} - {network.title}'))
+                else:
+                    self.stdout.write(self.style.SUCCESS(f'Updated network: {network.id} - {network.title}'))
