55229ff1aa07ac9945194f9720f7559e46e085d9
Anton <ieasycat@hotmail.com>
2024-09-02 16:47:13 +0200

    Add a script and data for this operation, which creates crypto models

---
+++ sitecrypto/crypto/management/commands/import_crypto.py
@@ -0,0 +1,28 @@
+import json
+from django.core.management.base import BaseCommand
+from crypto.models import Crypto, Network
+
+
+class Command(BaseCommand):
+    help = 'Load crypto data from JSON file'
+
+    def add_arguments(self, parser):
+        parser.add_argument('json_file', type=str)
+
+    def handle(self, *args, **kwargs):
+        json_file = kwargs['json_file']
+        with open(json_file, 'r') as file:
+            data = json.load(file)
+
+            for item in data:
+                networks = Network.objects.filter(id__in=item['networks'])
+                crypto, created = Crypto.objects.update_or_create(
+                    title=item['title'],
+                    slug=item['slug'],
+                    full_name=item['full_name'],
+                    defaults={
+                        'content': item['content'],
+                    }
+                )
+                crypto.networks.set(networks)
+                self.stdout.write(self.style.SUCCESS(f'Processed crypto: {item["title"]}'))
